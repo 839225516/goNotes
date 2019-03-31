ca741192aa6a99c5744a4732c5916d5789d7bba5
zengxianglin <839225516@qq.com>
2019-03-31 22:22:05 +0800

    golang 处理表单的输入

---
+++ web_proj/03-form/01-input/login.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "hello golang")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	// 获取请求方法
+	fmt.Println("method:", r.Method)
+
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		// 请求的是登录数据，那么执行登录的逻辑判断
+
+		// 解析表单，不加这个是不会表单作操作
+		r.ParseForm()
+
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("passwd:", r.Form["password"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9000", nil)
+
+	if err != nil {
+		log.Fatal("listenAndServe:", err)
+	}
+}
