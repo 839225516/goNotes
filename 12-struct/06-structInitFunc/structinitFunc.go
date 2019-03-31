c306753697301f7b7f505a6773e13be6eb0f89a7
zengxianglin <839225516@qq.com>
2019-03-31 18:05:43 +0800

    结构体初始化操作的函数封装

---
+++ 12-struct/06-structInitFunc/structinitFunc.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// go 的类型或结构体没有构造函数的功能，初始化过程可以使用函数封装
+
+type Cat struct {
+	Color string
+	Name  string
+}
+
+func NewCatByName(name string) *Cat {
+	return &Cat{
+		Name: name,
+	}
+}
+
+func NewCatByColor(color string) *Cat {
+	return &Cat{
+		Color: color,
+	}
+}
+
+func main() {
+	var kity *Cat = NewCatByName("kitty")
+	var whiteCat *Cat = NewCatByColor("white")
+
+	fmt.Println(kity.Name)
+	fmt.Println(whiteCat)
+}
