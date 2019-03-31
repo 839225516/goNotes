fdce0c3d7f928eab091b3274fb69d0f3adbf913c
zengxianglin <839225516@qq.com>
2019-03-31 19:47:35 +0800

    非指针接器

---
+++ 12-struct/10-notPointMethod/notPointMethod.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	X int
+	Y int
+}
+
+//非指针接收器
+func (p Point) Add(other Point) Point {
+	return Point{p.X + other.X, p.Y + other.Y}
+}
+
+func main() {
+	p1 := Point{1, 1}
+	p2 := Point{2, 2}
+
+	result := p1.Add(p2)
+
+	fmt.Println(result)
+}
